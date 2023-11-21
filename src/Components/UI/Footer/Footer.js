6cef9dd33d56274ac7f09f073381424589e9f945
Kevin Yuen <90118768+yuenkev@users.noreply.github.com>
2023-11-21 18:45:31 -0500

    Updating Fade animations for footer

--- src/Components/UI/Footer/Footer.js
+++ src/Components/UI/Footer/Footer.js
@@ -3,16 +3,12 @@ import classes from "./Footer.module.css";
 import Info from "./Info";
 import Connect from "./Connect";
 
-import { Fade } from "react-awesome-reveal";
-
 const Footer = (props) => {
   return (
-    <Fade direction="down" duration={1000} triggerOnce="true">
       <div className={classes.footer} id="footer">
         <Info className={classes.info}></Info>
         <Connect className={classes.connect}></Connect>
       </div>
-    </Fade>
   );
 };
 
